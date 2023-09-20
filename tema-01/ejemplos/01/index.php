cdd6862a1d9c60d66cb22c4c319ad07c5e0d61f7
Adri <amergam1012@g.educaand.es>
2023-09-20 14:55:08 +0200

    Primer commit

---
+++ tema-01/ejemplos/01/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    /*
+
+        Ejemplo 1 - PHP
+        Hola mundo
+
+    */
+
+    echo '<h1>';
+    echo '<hr>';
+    echo '<center>';
+    echo 'Hola Mundo PHP';
+    echo '<BR>';
+    echo 'Adrián Merino Gamaza';
+    echo '</center>';
+    echo '<hr>';
+    echo '</h1>';
+    echo 'DWES - Curso 23/24'
+
+?>
